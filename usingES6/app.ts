2ff0239a63d3d44ec6520f70d2041414209cd8f3
Daniel Elebash <danelebash@hotmail.com>
2016-04-07 22:47:04 -0400

    tested working

--- usingES6/app.ts
+++ usingES6/app.ts
@@ -2,7 +2,7 @@ import greeter from './greeter';
 import $ from "jquery";  //Only if trageting ES6 in tsconfig
 
 $(() => {
-    var name = 'sdfasf';
+    var name = 'dan';
     $(document.body).html(greeter(name));
 });
 
